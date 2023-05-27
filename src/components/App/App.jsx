9cdbeb29be702c54cf6bad0f7821b0256667c987
ishu1997 <mightymajestic1997@gmail.com>
2023-05-27 14:41:50 +0530

    class compoent vs fucntional componet. and start od development

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+// class based
+class App extends React.Component {
+    render() {
+        return <h1>this is my functional component</h1>
+    }
+}
+
+
+// function based
+// function App() {
+//     return <h1>this is my functional component</h1>
+// }
+
+export default App;
