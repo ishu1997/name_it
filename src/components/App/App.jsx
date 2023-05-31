d7cb89d747da455177b6ac8ee1327fd31c033a99
ishu1997 <mightymajestic1997@gmail.com>
2023-05-31 17:05:06 +0530

    convert parent class based comp to fun based

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,46 +1,67 @@
-import React from 'react';
+import { React, useState } from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import Searchbox from '../SearchBox/Searchbox';
 import ResultsContainer from '../ResultsContainer/ResultsContainer';
 
+const name = require('@rstacruz/startup-name-generator');
 
 // class based
-class App extends React.Component {
+// class App extends React.Component {
 
-    name = require('@rstacruz/startup-name-generator');
+//     name = require('@rstacruz/startup-name-generator');
 
-    // state provided by react
-    state = {
-        headerText: 'Name It!',
-        headerExpanded: true,
-        suggestedNames: []
-    }
+//     // state provided by react
+//     state = {
+//         headerText: 'Name It!',
+//         headerExpanded: true,
+//         suggestedNames: []
+//     }
 
-    handleInputChange = (keyword) => {
-        this.setState({
-            headerExpanded: !keyword,
-            suggestedNames: keyword ? this.name(keyword) : []
-        });
+//     handleInputChange = (keyword) => {
+//         this.setState({
+//             headerExpanded: !keyword,
+//             suggestedNames: keyword ? this.name(keyword) : []
+//         });
 
-    }
+//     }
 
-    render() {
-        return (
-            <div>
-                {/* passing props as headerTitle variable */}
-                <Header headerExpanded={this.state.headerExpanded} headerTitle={this.state.headerText} />
-                <Searchbox HandleInputChange={this.handleInputChange} />
-                <ResultsContainer suggestedNames={this.state.suggestedNames} />
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 {/* passing props as headerTitle variable */}
+//                 <Header headerExpanded={this.state.headerExpanded} headerTitle={this.state.headerText} />
+//                 <Searchbox HandleInputChange={this.handleInputChange} />
+//                 <ResultsContainer suggestedNames={this.state.suggestedNames} />
+//             </div>
+//         )
+//     }
+// }
 
 
 // function based
-// function App() {
-//     return <h1>this is my functional component</h1>
-// }
+const App = () => {
+
+    const [headerText] = useState('Name It!');
+    const [headerExpanded, setHeaderExpanded] = useState(true);
+    const [suggestedNames, setSuggestedNames] = useState([]);
+
+
+    const handleInputChange = (keyword) => {
+
+        setHeaderExpanded(!keyword);
+        setSuggestedNames(keyword ? name(keyword) : []);
+
+    }
+
+    return (
+        <div>
+            {/* passing props as headerTitle variable */}
+            <Header headerExpanded={headerExpanded} headerTitle={headerText} />
+            <Searchbox HandleInputChange={handleInputChange} />
+            <ResultsContainer suggestedNames={suggestedNames} />
+        </div>
+    )
+};
 
 export default App;
